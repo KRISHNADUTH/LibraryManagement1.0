fc87eb3970dd0755242a673a79b535087bbbaefb
Krishnaduth <krishnaduth777@gmail.com>
2024-06-25 05:54:20 +0530

    Initial commit

---
+++ src/main/java/com/krishna/LibraryManageMent/LibraryManageMentApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.LibraryManageMent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManageMentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManageMentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishna/LibraryManageMent/LibraryManageMentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.LibraryManageMent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManageMentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
