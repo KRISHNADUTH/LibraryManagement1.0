b56a8a4e257826ed29476f2a949ae627acc6bcc3
Krishnaduth <krishnaduth777@gmail.com>
2024-06-26 06:53:19 +0530

    added model details to problem statement

--- src/main/java/com/krishna/LibraryManageMent/LibraryManageMentApplication.java
+++ src/main/java/com/krishna/LibraryManageMent/LibraryManageMentApplication.java
@@ -1,13 +1,22 @@
 package com.krishna.LibraryManageMent;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryManageMentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManageMentApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return runner -> {
+			System.out.println("Hello world..............");
+		};
 	}
 
 }
